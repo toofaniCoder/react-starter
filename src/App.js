e5c1a3b76b6ace28a969d1b02f15f39b714bee8e
toofaniCoder <smartcoder500@gmail.com>
2020-10-08 20:16:36 +0530

    add bootstrap support with sass

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container py-5">
+        <div className="card border-0 shadow bg-primary text-white text-center p-4 w-25 mx-auto">
+          <h4>Bootstrap</h4>
+          <p className="m-0">powerd by reactjs</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
